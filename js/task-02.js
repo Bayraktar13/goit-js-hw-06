c59a4af4ace0092d1108dd77db2d061ec646c4e9
Bayraktar13 <andriihrytsenko@gmail.com>
2023-06-23 16:20:57 +0200

    ДЗ-6/2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ul = document.querySelector("#ingredients");
+
+function createList(names) {
+  names.forEach((el) => {
+    const li = document.createElement("li");
+    li.classList.add("item");
+    li.textContent = el;
+    ul.append(li);
+  });
+}
+
+createList(ingredients);
