99df28b7e3495cbcebc16109840228e630af5e04
Bayraktar13 <andriihrytsenko@gmail.com>
2023-06-26 15:38:25 +0200

    ДЗ-6/8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const emailInput = form.elements.email;
+  const passwordInput = form.elements.password;
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("Все поля должны быть заполнены.");
+    return;
+  }
+  const formData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+  console.log(formData);
+  form.reset();
+});
+
 
