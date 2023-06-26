5042d0456928bbf15eec1fbf4788ae0ea3a69c25
Bayraktar13 <andriihrytsenko@gmail.com>
2023-06-26 16:03:58 +0200

    ДЗ-6/9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeColorButton = document.querySelector(".change-color");
+const spanElement = document.querySelector(".color");
+const bodyElement = document.body;
+
+changeColorButton.addEventListener("click", (event) => {
+  const randomColor = getRandomHexColor();
+  bodyElement.style.background = randomColor;
+  spanElement.textContent = randomColor;
+});
