4ba1d502f562a0170f8162f5027bad0002a53b61
Bayraktar13 <andriihrytsenko@gmail.com>
2023-06-27 13:33:53 +0200

    ДЗ-6/8 доработка

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,41 @@
+// const form = document.querySelector(".login-form");
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const emailInput = form.elements.email;
+//   const passwordInput = form.elements.password;
+//   if (emailInput.value === "" || passwordInput.value === "") {
+//     alert("Все поля должны быть заполнены.");
+//     return;
+//   }
+//   const formData = {
+//     email: emailInput.value,
+//     password: passwordInput.value,
+//   };
+//   console.log(formData);
+//   form.reset();
+// });
+
+
 const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const emailInput = form.elements.email;
-  const passwordInput = form.elements.password;
+
+  const emailInput = event.target.elements.email;
+  const passwordInput = event.target.elements.password;
+
   if (emailInput.value === "" || passwordInput.value === "") {
-    alert("Все поля должны быть заполнены.");
+    alert("Всі поля повинні бути заповнені.");
     return;
   }
+
   const formData = {
     email: emailInput.value,
     password: passwordInput.value,
   };
-  console.log(formData);
-  form.reset();
-});
 
+  console.log(formData);
+  event.target.reset();
+});
 
